a3f5300a1c48e7b0bf55b24411e24329881f5228
paaavkata <paaavkata@gmail.com>
2017-04-24 23:57:11 +0300

    First conflict done

--- src/main/java/com/freeagents/controller/UserController.java
+++ src/main/java/com/freeagents/controller/UserController.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.WebRequest;
